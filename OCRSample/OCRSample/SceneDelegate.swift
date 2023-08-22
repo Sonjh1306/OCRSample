195a42034fbba5e9057a6fd15d46a743018541e5
Sonjh1306 <73586326+Sonjh1306@users.noreply.github.com>
2023-08-22 09:31:04 +0900

    프로젝트 생성

---
+++ OCRSample/OCRSample/SceneDelegate.swift
@@ -0,0 +1,14 @@
+//
+//  SceneDelegate.swift
+//  OCRSample
+//
+//  Created by sonjuhyeong on 2023/08/22.
+//
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+
+    var window: UIWindow?
+}
+
---
+++ OCRSample/OCRSample/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  OCRSample
+//
+//  Created by sonjuhyeong on 2023/08/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    
+    }
+
+
+}
+
